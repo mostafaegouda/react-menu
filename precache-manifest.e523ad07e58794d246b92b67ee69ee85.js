79ab1393c8c114d60fc5d46176fdfe8a7d7061cd
Mostafa Gouda <mostafaegouda@gmail.com>
2022-05-23 17:15:49 +0200

    Updates

---
+++ precache-manifest.e523ad07e58794d246b92b67ee69ee85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b09d040119b5afa12f63980b2992910f",
+    "url": "/react-menu/index.html"
+  },
+  {
+    "revision": "1d4335d0ac7ba132b19f",
+    "url": "/react-menu/static/css/main.9c6ab881.chunk.css"
+  },
+  {
+    "revision": "c3bc376523061cbef635",
+    "url": "/react-menu/static/js/2.15f06c35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-menu/static/js/2.15f06c35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d4335d0ac7ba132b19f",
+    "url": "/react-menu/static/js/main.13dde37f.chunk.js"
+  },
+  {
+    "revision": "16969508fe17c64f3354",
+    "url": "/react-menu/static/js/runtime-main.a64c297e.js"
+  }
+]);
